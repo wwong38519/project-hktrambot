71cd0155c7739875f9f81b39ce02ae147f326bb5
wwong38519 <wwong38519@gmail.com>
2016-08-02 01:13:41 +0800

    show special message show estimated arrival time for last chosen stop

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ TIMEOUT_SEC = 30
 msgKeyboardStart = '/start'
 msgKeyboardCancel = '/cancel'
 msgKeyboardFindStop = 'Find my nearest stop'
+msgKeyboardShowEta = 'Show estimated arrival time'
 msgKeyboardDirectionEast = 'East'
 msgKeyboardDirectionWest = 'West'
 msgReady = 'Please send '+msgKeyboardStart+' to start.'
@@ -42,7 +43,12 @@ def get_nearest_stop(coordinates, direction):
 def get_eta(code):
 	response = requests.get(ENDPOINT_ETA+code)
 	data = json.loads(response.text)
-	records = [ (record['$']['tram_dest_tc'], record['$']['tram_dest_en'], record['$']['arrive_in_minute']+' min') for record in data['root']['metadata'] ]
+	records = []
+	for record in data['root']['metadata'] :
+		if record['$'].has_key('special_msg_id'):
+			records.append((record['$']['msg_tc'], record['$']['msg_en']))
+		else:
+			records.append((record['$']['tram_dest_tc'], record['$']['tram_dest_en'], record['$']['arrive_in_minute']+' min'))
 	print('Eta retrieved for stop '+code)
 	return records
 
@@ -60,12 +66,18 @@ keyboardLocation = ReplyKeyboardMarkup(keyboard=[
 	[KeyboardButton(text=msgKeyboardCancel)],
 ], resize_keyboard=True)
 
+keyboardEta = ReplyKeyboardMarkup(keyboard=[
+	[KeyboardButton(text=msgKeyboardShowEta)],
+	[KeyboardButton(text=msgKeyboardStart)],
+	[KeyboardButton(text=msgKeyboardCancel)],
+], resize_keyboard=True)
+
 class MessageBot(telepot.helper.ChatHandler):
 	def __init__(self, seed_tuple, timeout):
 		super(MessageBot, self).__init__(seed_tuple, timeout)
 	def open(self, initial_msg, seed):
 		self._alive = True
-		self._direction = ''
+		self.reset()
 		# return True
 	def on_message(self, msg):
 		text = msg.get('text', None)
@@ -89,22 +101,33 @@ class MessageBot(telepot.helper.ChatHandler):
 				replyMsg = msgLocation
 				keyboard = keyboardLocation
 				self.sender.sendMessage(replyMsg, reply_markup=keyboard)
+			elif text == msgKeyboardShowEta:
+				replyMsg = self.get_eta(self._stop)
+				keyboard = keyboardEta
+				self.sender.sendMessage(replyMsg, reply_markup=keyboard)
 		elif location is not None:
 			coordinates = (location['latitude'], location['longitude'])
 			(code, names, coord) = get_nearest_stop(coordinates, self._direction)
-			records = get_eta(code)
-			replyMsg = u' / '.join(names) + '\n'
-			replyMsg = replyMsg + u'\n'.join([ u' / '.join(e) for e in records ])
-			keyboard = keyboardDirection
+			self._stop = (code, names, coord)
+			replyMsg = self.get_eta(self._stop)
+			keyboard = keyboardEta
 			self.sender.sendMessage(replyMsg, reply_markup=keyboard)
 			self.sender.sendLocation(coord[0], coord[1], reply_markup=keyboard)
 	def on_close(self, exception):
 		if self._alive:
 			self._alive = False
-			self._direction = ''
+			self.reset()
 			replyMsg = msgReady
 			keyboard = keyboardReady
 			self.sender.sendMessage(replyMsg, reply_markup=keyboard)
+	def reset(self):
+		self._direction = None
+		self._stop = None
+	def get_eta(self, (code, names, coord)):
+		records = get_eta(code)
+		replyMsg = u' / '.join(names) + '\n'
+		replyMsg = replyMsg + u'\n'.join([ u' / '.join(e) for e in records ])
+		return replyMsg
 
 if get_stops(): 
 	bot = telepot.DelegatorBot(TOKEN, [
